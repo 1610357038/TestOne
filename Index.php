7a26c996da7794f56143e936f74b1d33fd86d1ef
Administrator <753132377@qq.com>
2016-12-12 08:24:16 +0800

    Initial commit

---
+++ Index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/12/12
+ * Time: 8:23
+ */
+class Index
+{
+    public  function  index(){
+        echo '哈哈哈哈哈小智障';
+    }
+
+}
